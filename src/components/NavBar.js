f54662241b7cd606538c1211356a901729ff330d
townsend75 Townsend <dtownsend@gmx.net>
2023-12-06 10:48:43 +0000

    create hook to close navbar dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,20 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/ArtXChangeLogo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
 import axios from "axios";
 import Avatar from "./Avatar";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
-const currentUser = useCurrentUser();
-const setCurrentUser = useSetCurrentUser();
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
+  
 
   const handleSignOut = async () => {
     try {
@@ -77,7 +84,12 @@ const setCurrentUser = useSetCurrentUser();
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -85,7 +97,11 @@ const setCurrentUser = useSetCurrentUser();
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
