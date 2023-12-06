c7a37d109e8ba76f2ca99f0574c9f7c28d0c268a
townsend75 Townsend <dtownsend@gmx.net>
2023-12-06 09:57:53 +0000

    add context folder for current user state

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://pp5-drf-7cd889e92857.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
